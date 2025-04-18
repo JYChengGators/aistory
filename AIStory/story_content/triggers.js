d9317e6e1acac86223c20af241674a59848ba9ae
JYChengGators <chengjiunyao@ufl.edu>
2025-04-18 04:23:28 -0400

    Story update

---
+++ AIStory/story_content/triggers.js
@@ -0,0 +1,80 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6mrnXAVlbln":
+        Script1();
+        break;
+      case "6dQjT3jFtdm":
+        Script2();
+        break;
+      case "5itOsKLbEzU":
+        Script3();
+        break;
+      case "6jdIQHtF4Ui":
+        Script4();
+        break;
+      case "5q2b2yWaXNE":
+        Script5();
+        break;
+      case "6OwgA3RgcA7":
+        Script6();
+        break;
+      case "606URsrJo9z":
+        Script7();
+        break;
+      case "5zPeiFCfm1i":
+        Script8();
+        break;
+      case "6TZvow7UbLC":
+        Script9();
+        break;
+      case "6nVA8Ij4Vsn":
+        Script10();
+        break;
+      case "5o245PTq2cp":
+        Script11();
+        break;
+      case "6gbZC8tDrjU":
+        Script12();
+        break;
+      case "6K5yCFFFmNK":
+        Script13();
+        break;
+      case "6AUFjPqlx7Y":
+        Script14();
+        break;
+      case "6nhtLwtdHL8":
+        Script15();
+        break;
+      case "5sVHDm7z0VU":
+        Script16();
+        break;
+      case "5mhkZYSsw67":
+        Script17();
+        break;
+      case "6dKC63tlt6f":
+        Script18();
+        break;
+      case "6UD5QZYEbBM":
+        Script19();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+};
