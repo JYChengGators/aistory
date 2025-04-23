dff45870f3b90f2cf5a40633656171060f45ecfe
JYChengGators <chengjiunyao@ufl.edu>
2025-04-23 19:03:52 -0400

    final version

--- story_content/triggers.js
+++ story_content/triggers.js
@@ -2,61 +2,61 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6WkDjltKpPO":
+      case "6eppPSffIG5":
         Script1();
         break;
-      case "6UoEYsSBPHF":
+      case "5hoBskEh0Ao":
         Script2();
         break;
-      case "6B0Los2tV5l":
+      case "6RPYlnAHjoI":
         Script3();
         break;
-      case "5vV2armXUCW":
+      case "5raCpezYYhf":
         Script4();
         break;
-      case "6CfAQeQ8sEK":
+      case "5mcCFNWbjml":
         Script5();
         break;
-      case "6nJTA6yexoL":
+      case "6adC1pkZjzM":
         Script6();
         break;
-      case "5hYHl7nBzIH":
+      case "5cW6nwmNPG7":
         Script7();
         break;
-      case "5wgfDTHI1QI":
+      case "66GAxAAknS7":
         Script8();
         break;
-      case "6BGjeHDASUS":
+      case "61MiZdmjvJt":
         Script9();
         break;
-      case "6af2dvFSsfW":
+      case "6Iie6ZDYrQm":
         Script10();
         break;
-      case "5vS7h00tDrW":
+      case "6goZHLvYwAp":
         Script11();
         break;
-      case "5WJSyvMKRZ1":
+      case "6aPAd75f1zh":
         Script12();
         break;
-      case "5VJg53CzozT":
+      case "6IZ4IXfeu2c":
         Script13();
         break;
-      case "5YtV6VSCVXi":
+      case "6UxeAD4WDT4":
         Script14();
         break;
-      case "5bhTb9cAxi7":
+      case "6HLPisiv4eH":
         Script15();
         break;
-      case "5oxYnz3ft7t":
+      case "6JdWfytKQsE":
         Script16();
         break;
-      case "6Q6gZ2d36sD":
+      case "6hRKqt3utEb":
         Script17();
         break;
-      case "6QajzVwofnJ":
+      case "5mFgg82l9Oy":
         Script18();
         break;
-      case "6VpJrjpCWQn":
+      case "5d7k9S9BsMK":
         Script19();
         break;
   }
